4ee603aa308f2aad8f2f47adc05c094cc270f4c1
Maurilio Mendes <mauriliomendes2024@gmail.com>
2024-05-04 15:18:56 -0300

    Aleracao 04.05.2024

--- src/main/java/br/com/fuctura/aplicacao/AssociacoesAplicacao.java
+++ src/main/java/br/com/fuctura/aplicacao/AssociacoesAplicacao.java
@@ -47,7 +47,7 @@ public class AssociacoesAplicacao {
 		
 		for(Veiculo vv : resultado) {
 			System.out.println(" Modelo: " + vv.getModelo() + " \n Loja: "+ vv.getLoja().getNome());
-			
+			System.out.println("Maurilio");
 			
 		}
 		
